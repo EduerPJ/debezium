4c6bc46b602263fba0bd71933ced57cb178bf3d4
Eduer Pallares <eduer.pallares@zalvadora.com>
2025-06-09 21:57:37 -0500

    Actualiza el script del conector JDBC Sink para MySQL, cambiando la clase del conector a "io.confluent.connect.jdbc.JdbcSinkConnector", ajustando las credenciales de conexión y simplificando la lista de temas. También se elimina la lista de exclusión de tablas en el conector MySQL fuente.

--- create-jdbc-mysql-sink-connector.sh
+++ create-jdbc-mysql-sink-connector.sh
@@ -7,17 +7,17 @@ curl -X POST http://localhost:8083/connectors \
   -d '{
     "name": "jdbc-connector",
     "config": {
-        "connector.class": "io.debezium.connector.jdbc.JdbcSinkConnector",
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
         "tasks.max": "1",
         "connection.url": "jdbc:mysql://localhost:3306/jdbc_copia",
-        "connection.username": "debezium",
-        "connection.password": "dbz",
+        "connection.username": "root",
+        "connection.password": "",
         "insert.mode": "upsert",
         "delete.enabled": "true",
-        "primary.key.mode": "record_key",
+        "pk.mode": "record_key",
         "schema.evolution": "basic",
         "use.time.zone": "America/Bogota",
-        "topics": "Analytic.zalvadora_local_2.users",
+        "topics": "users",
         "field.include.list": "id, firstname, lastname, email, status"
     }
   }'
--- create-mysql-source-connector.sh
+++ create-mysql-source-connector.sh
@@ -17,7 +17,6 @@ curl -X POST http://localhost:8083/connectors \
       "topic.prefix": "Analyticdb1",
       "database.include.list": "zalvadora_local_2",
       "table.include.list": "zalvadora_local_2.users",
-      "table.exclude.list": "zalvadora_local_2\\.*",
       "column.include.list": "id, firstname, lastname, email, status",
       "schema.history.internal.kafka.bootstrap.servers": "kafka:9092",
       "schema.history.internal.kafka.topic": "schema-changes.zalvadora_local_2",
